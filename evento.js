5a329b9b17affea922e2e63f670f294f3d8ab9d0
elavamosnos <kanxa.tfj@gmail.com>
2021-08-24 19:47:39 -0300

    https://github.com/elavamosnos/AtividadeUc8.git

---
+++ evento.js
@@ -0,0 +1,22 @@
+console.log("Bem vindo")
+let data = "23/08/2021"
+if( data => "24/08/2021") {
+    console.log("data inválida");
+} else{
+}
+let cadastrados = ["Caio" , "Leticia" , "Fabricio" , "Eduardo"];
+    if( cadastrados.length < 100); {
+        console.log("Digite seu nome");
+    
+        console.log("Digite sua idade");
+    }
+let idade = 18 
+    if( idade >18)
+    console.log("idade não permitida");
+    if( cadastrados.length <100); {
+        console.log("cadastro realizado com sucesso");
+    }
+    if( cadastrados.length >100); {
+        console.log("excedeu o numero de vagas") 
+    
+    }
